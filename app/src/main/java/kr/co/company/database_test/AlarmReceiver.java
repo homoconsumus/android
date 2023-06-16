da84e7fcfa487a97d39ada879bf9b1d1e1a24a13
Kumiori77 <dannylee88@naver.com>
2023-06-16 09:25:06 +0900

    edit main page

--- app/src/main/java/kr/co/company/database_test/AlarmReceiver.java
+++ app/src/main/java/kr/co/company/database_test/AlarmReceiver.java
@@ -1,4 +1,13 @@
 package kr.co.company.database_test;
 
-public class AlarmReceiver {
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
 }
