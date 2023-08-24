b6402ab088700460d26d74e0983d132f57bb736e
AI510-26 <dannylee88@naver.com>
2023-08-24 13:12:06 +0900

    AlarmReceiver 클래스 추가 & 매인 화면에 알림 시간 표시 업데이트 2023.08.24

---
+++ app/src/main/java/kr/co/company/database_test/AlarmReceiver.java
@@ -0,0 +1,13 @@
+package kr.co.company.database_test;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
